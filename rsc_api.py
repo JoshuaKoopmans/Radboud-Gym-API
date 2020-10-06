e7eea9a84a95b34ef0c2ed6ca1c1b37aea4b9410
Joshua Koopmans <31856090+JoshuaKoopmans@users.noreply.github.com>
2020-10-06 16:46:12 +0200

    Update rsc_api.py

--- rsc_api.py
+++ rsc_api.py
@@ -2,6 +2,7 @@ import requests, json, time, random, getpass
 
 ENDPOINT = "https://publiek.usc.ru.nl/app/api/v1/?module={}&method={}&lang=nl"
 
+
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -12,57 +13,63 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
+    
 def login(username, password):
     
-    data = {'username':username,
-            'password':password}
+    data = {'username': username,
+            'password': password}
 
-    r = requests.post(url=ENDPOINT.format('user','logIn'), data=data)
+    r = requests.post(url=ENDPOINT.format('user', 'logIn'), data=data)
     return json.loads(r.text)
     
 
 def logout(klant_id, token):
-    data = {'klantId':klant_id,
-            'token':token}
+    data = {'klantId': klant_id,
+            'token': token}
 
-    r = requests.post(url=ENDPOINT.format('user','logOut'), data=data)
+    r = requests.post(url=ENDPOINT.format('user', 'logOut'), data=data)
     return json.loads(r.text)
+
 
 def get_user_agenda(klant_id, token):
-    data = {'klantId':klant_id,
-            'token':token}
-    r = requests.post(url=ENDPOINT.format('agenda','getAgenda'), data=data)
+    data = {'klantId': klant_id,
+            'token': token}
+    r = requests.post(url=ENDPOINT.format('agenda', 'getAgenda'), data=data)
     return json.loads(r.text)
 
+
 def get_user_info(klant_id, token):
-    data = {'klantId':klant_id,
-            'token':token}
-    r = requests.post(url=ENDPOINT.format('user','getUserData'), data=data)
+    data = {'klantId': klant_id,
+            'token': token}
+    r = requests.post(url=ENDPOINT.format('user', 'getUserData'), data=data)
     return json.loads(r.text)
 
 def get_available_subscriptions(klant_id, token):
-    data = {'klantId':klant_id,
-            'token':token}
-    r = requests.post(url=ENDPOINT.format('locatie','getLocaties'), data=data)
+    data = {'klantId': klant_id,
+            'token': token}
+    r = requests.post(url=ENDPOINT.format('locatie', 'getLocaties'), data=data)
     return json.loads(r.text)
+
 
 def add_subscription(klant_id, token, inschrijving_id, pool_id, laanbod_id, start, eind):
-    data = {'klantId':klant_id,
-            'token':token,
+    data = {'klantId': klant_id,
+            'token': token,
             'inschrijvingId': inschrijving_id,
-            'poolId':pool_id,
-            'laanbodId':laanbod_id,
-            'start':start,
-            'eind':eind}
-    r = requests.post(url=ENDPOINT.format('locatie','addLinschrijving'), data=data)
+            'poolId': pool_id,
+            'laanbodId': laanbod_id,
+            'start': start,
+            'eind': eind}
+    r = requests.post(url=ENDPOINT.format('locatie', 'addLinschrijving'), data=data)
     return json.loads(r.text)
 
+
 def delete_subscription(klant_id, token, linschrijving_id):
-    data = {'klantId':klant_id,
-            'token':token,
+    data = {'klantId': klant_id,
+            'token': token,
             'linschrijvingId': linschrijving_id}
-    r = requests.post(url=ENDPOINT.format('locatie','deleteLinschrijving'), data=data)
+    r = requests.post(url=ENDPOINT.format('locatie', 'deleteLinschrijving'), data=data)
     return json.loads(r.text)
+
 
 username = str(input("Enter Username: "))
 password = getpass.getpass("Enter Password: ")
